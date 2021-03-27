407f74b4de34af0f674b7e8583e76a5707f52e14
Leonardo Kobylinski <54051374+leokoby@users.noreply.github.com>
2021-03-27 17:52:32 -0300

    2#commit

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,12 @@
 const btnMenu = document.querySelector('.btn-menu i')
 const menu = document.querySelector('.menu-container')
 
+
 btnMenu.addEventListener('click', menuMobile)
+
 
 function menuMobile(){
     menu.classList.toggle('openMenu')
     btnMenu.classList.toggle('fa-times')
 }
+
