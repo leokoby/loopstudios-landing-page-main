a75cfed7ae78c3f54f3cd99f430975a79e6b7305
Leonardo Kobylinski <54051374+leokoby@users.noreply.github.com>
2021-03-27 14:44:36 -0300

    2#commit
    add responsive

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,9 @@
-const btnMenu = document.querySelector('.btn-menu')
+const btnMenu = document.querySelector('.btn-menu i')
 const menu = document.querySelector('.menu-container')
 
 btnMenu.addEventListener('click', menuMobile)
 
 function menuMobile(){
     menu.classList.toggle('openMenu')
+    btnMenu.classList.toggle('fa-times')
 }
